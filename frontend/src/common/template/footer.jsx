5c0fd7ae0b07a6a05beb2123bae96fcbe2a792e3
vitor9 <vtorzy@gmail.com>
2018-07-13 16:22:05 -0300

    S9 A110 Componente rodapé

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href='https://github.com/vitor9' target='_blank'> dev_Vitor</a>
+        </strong>
+    </footer>
+)
