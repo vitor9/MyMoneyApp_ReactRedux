7eee99f44c378e4c9f3dc4b0cca222cc951a1922
vitor9 <vtorzy@gmail.com>
2018-07-13 15:19:21 -0300

    S9 A108 - Componente Menu pt1

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,11 @@ import '../common/template/dependencies'
 import React from 'react'
 
 import Header from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
 
 export default props => (
     <div className='wrapper'>
         <Header />
+        <Sidebar />
     </div>
 )
