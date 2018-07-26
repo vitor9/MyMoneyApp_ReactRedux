9588780c54b3d5fdf3668219ffbeea8eade9ee1b
vitor9 <vtorzy@gmail.com>
2018-07-26 18:50:19 -0300

    S10 A135 - Criando Action "getList" e o Reducer.

---
+++ frontend/src/billingCycle/billingCycleReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = { list: [] }
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        // Utilizamos o operador spread(...) para poder replicar 
+        // o estado atual e garantir tudo o que tinha antes que fique aqui.
+        case 'BILLING_CYCLES_FETCHED':
+            return { ...state, list: action.payload }
+        default:
+            return state
+    }
+}
