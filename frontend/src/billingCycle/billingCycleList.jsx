adf7f9550d076210a5fe926ab53c350ec66c4c3f
vitor9 <vtorzy@gmail.com>
2018-07-27 12:05:37 -0300

    S10 A136 - Integrando BillingCycleList com Redux.

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getList } from './billingCycleActions'
 
 class BillingCycleList extends Component {
+
+    componentWillMount() {
+        this.props.getList()
+    }
+    
     render() {
+        console.log(this.props.list);
         return (
             <div>
                 <table className='table'>
@@ -21,5 +30,14 @@ class BillingCycleList extends Component {
         )
     }
 }
+// Este metodo recebe por parametro um estado e vai retornar um objeto
+// que o objeto eh o mapeamento do que eu quero colocar dentro das propriedades dentro
+// do nosso componente atual(billingCycleList) e onde no estado este dado vai estar presente
+// para que eu jogue nas propriedades. Do lado direito deste objeto, vai ser os valores
+// que vao ser colocados na propriedade do componente.
+const mapStateToProps = state => ({ list: state.billingCycle.list })
 
-export default BillingCycleList
+// Dispatch eh quem dispara a acao para os reducers. Com ele, fazemos o bind
+// de nosso action creator
+const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
