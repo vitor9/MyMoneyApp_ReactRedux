764fd5b8730c30841ec85297cfdc7263740ebdd6
vitor9 <vtorzy@gmail.com>
2018-07-27 12:29:41 -0300

    S10 A137 - Exibindo os dados em BillingCycleList.

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -9,6 +9,17 @@ class BillingCycleList extends Component {
         this.props.getList()
     }
     
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{ bc.name }</td>
+                <td>{ bc.month }</td>
+                <td>{ bc.year }</td>
+            </tr>
+        ))
+    }
+
     render() {
         console.log(this.props.list);
         return (
@@ -23,7 +34,7 @@ class BillingCycleList extends Component {
                     </thead>
 
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
